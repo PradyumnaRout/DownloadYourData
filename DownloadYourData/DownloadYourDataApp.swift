c04b58fc5ad9584408bcbaa1e8bbe2d40692d58b
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-07-17 12:17:30 +0530

    Initial Commit

---
+++ DownloadYourData/DownloadYourDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadYourDataApp.swift
+//  DownloadYourData
+//
+//  Created by hb on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DownloadYourDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
